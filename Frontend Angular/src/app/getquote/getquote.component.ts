63e7c9967bac39a899dabd63b55bc757e46175a7
mouadbroos <mouadbroos@gmail.com>
2018-09-24 09:13:41 -0400

    added sender/receivreceiver + shippment information

--- Frontend Angular/src/app/getquote/getquote.component.ts
+++ Frontend Angular/src/app/getquote/getquote.component.ts
@@ -20,9 +20,46 @@ export class GetquoteComponent implements OnInit {
   public searchControl: FormControl;
   public zoom: number;
 
-  @ViewChild("search")
-  public searchElementRef: ElementRef;
+  public shippment = {
+    date: '',
+    quantity:0,
+    weight:0
+  };   
+ 
+  public componentForm = {
+    street_number: 'short_name',
+    route: 'long_name',
+    locality: 'long_name',
+    administrative_area_level_1: 'short_name',
+    country: 'long_name',
+    postal_code: 'short_name'
+  };
 
+  public contentForm1 = {
+    name :'',
+    street_number: '',
+    route: '',
+    locality: '',
+    administrative_area_level_1: '',
+    country: '',
+    postal_code: ''
+  };
+  
+  public contentForm2 = {
+    name :'',
+    street_number: '',
+    route: '',
+    locality: '',
+    administrative_area_level_1: '',
+    country: '',
+    postal_code: ''
+  };  
+
+  @ViewChild("search1")
+  public searchElementRef1: ElementRef;
+
+  @ViewChild("search2")
+  public searchElementRef2: ElementRef;
 
 
   ngOnInit() {
@@ -39,13 +76,21 @@ export class GetquoteComponent implements OnInit {
 
     //load Places Autocomplete
     this.mapsAPILoader.load().then(() => {
-      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef1.nativeElement, {
         types: ["address"]
       });
       autocomplete.addListener("place_changed", () => {
         this.ngZone.run(() => {
           //get the place result
-          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();       
+
+          for (var i = 0; i < place.address_components.length; i++) {
+              var addressType = place.address_components[i].types[0];
+              if (this.componentForm[addressType]) {
+                var val = place.address_components[i][this.componentForm[addressType]];
+                this.contentForm1[addressType] = val;
+              }   
+            }
 
           //verify result
           if (place.geometry === undefined || place.geometry === null) {
@@ -59,6 +104,38 @@ export class GetquoteComponent implements OnInit {
         });
       });
     });
+
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef2.nativeElement, {
+        types: ["address"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          for (var i = 0; i < place.address_components.length; i++) {
+            var addressType = place.address_components[i].types[0];
+            if (this.componentForm[addressType]) {
+              var val = place.address_components[i][this.componentForm[addressType]];
+              this.contentForm2[addressType] = val;
+            }   
+          }
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
+
   }
 
   private setCurrentPosition() {
