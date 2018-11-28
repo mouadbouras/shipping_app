81cead022b62b7e0eebafb146a8cbd69a73e8d49
mbouras <mbouras@matrox.com>
2018-11-28 18:02:29 -0500

    added paypal payment sample

--- Frontend Angular/src/app/app.component.ts
+++ Frontend Angular/src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked  } from '@angular/core';
+
+declare let paypal: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked {
   title = 'shipping-app';
+
+  addScript: boolean = false;
+  paypalLoad: boolean = true;
+  
+  finalAmount: number = 1;
+
+  paypalConfig = {
+    env: 'sandbox',
+
+    style: {
+      layout: 'vertical',  // horizontal | vertical
+      size:   'medium',    // medium | large | responsive
+      shape:  'rect',      // pill | rect
+      color:  'gold'       // gold | blue | silver | black
+    },
+
+    client: {
+      sandbox: 'AWRVQkrwfja1nTxBmCmf5-NZSpqEN54cDWqCjopM7k5e-rrootzxQePt_gdo4rp4ZdX6gCijXREphUTZ',
+      production: 'AWRVQkrwfja1nTxBmCmf5-NZSpqEN54cDWqCjopM7k5e-rrootzxQePt_gdo4rp4ZdX6gCijXREphUTZ'
+    },
+    commit: true,
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            { amount: { total: this.finalAmount, currency: 'USD' } }
+          ]
+        }
+      });
+    },
+    onAuthorize: (data, actions) => {
+      return actions.payment.execute().then((payment) => {
+        //Do something when payment is successful.
+      })
+    }
+  };
+
+  ngAfterViewChecked(): void {
+    if (!this.addScript) {
+      this.addPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
+        this.paypalLoad = false;
+      })
+    }
+  }
+  
+  addPaypalScript() {
+    this.addScript = true;
+    return new Promise((resolve, reject) => {
+      let scripttagElement = document.createElement('script');    
+      scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scripttagElement.onload = resolve;
+      document.body.appendChild(scripttagElement);
+    })
+  }
+
 }
